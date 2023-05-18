5b659374dd3b7acb2a8e2596550055aab019fb22
Hoàng Anh Quân <kakaka021202@gmail.com>
2023-05-18 11:08:26 +0700

    Template driven - forms ngSubmit - submit ngModel - [ngModel] - [(ngModel)]

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,16 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'template_driven_forms';
+  userInfo = {
+    userName: 'quanha',
+    password: '',
+    rememberMe: true,
+  };
+
+  constructor(){}
+
+  onSubmit(form: NgForm) : void {
+    console.log(form);
+    throw new Error("Somthing went wrong!");
+  }
 }
