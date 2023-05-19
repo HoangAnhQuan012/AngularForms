e31a771effa6a32066e91d9dfbcac81ee2955e7d
Hoàng Anh Quân <kakaka021202@gmail.com>
2023-05-19 17:51:24 +0700

    Reactive Forms

---
+++ Forms_Angular/Reactive_forms/reactive_forms/src/app/app.module.ts
@@ -0,0 +1,33 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { ReactiveFormsModule } from '@angular/forms';
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { SignInComponent } from './sign-in/sign-in.component';
+import { MatInputModule } from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatCheckboxModule } from '@angular/material/checkbox';
+import { MatButtonModule } from '@angular/material/button';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+      SignInComponent
+   ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatButtonModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatCheckboxModule,
+    MatButtonModule,
+    ReactiveFormsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ Forms_Angular/Reactive_forms/reactive_forms/src/app/sign-in/sign-in.component.ts
@@ -0,0 +1,45 @@
+import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl, FormBuilder, Validators } from '@angular/forms';
+
+@Component({
+  selector: 'app-sign-in',
+  templateUrl: './sign-in.component.html',
+  styleUrls: ['./sign-in.component.css']
+})
+export class SignInComponent implements OnInit {
+
+  // form = new FormGroup({
+  //   username: new FormControl(''),
+  //   password: new FormControl(''),
+  //   rememberMe: new FormControl(false)
+  // });
+
+  form: FormGroup;
+  constructor(
+    private formBuilder: FormBuilder
+  ) { }
+
+  ngOnInit() {
+    this.createForms();
+    setTimeout(() => {
+      this.form.patchValue({
+        username: 'admin',
+        password: 'admin',
+        rememberMe: true
+      })
+    }, 1000);
+  }
+
+  createForms() {
+    this.formBuilder.group({
+      usename: ['', Validators.required],
+      password: ['', Validators.required],
+      rememberMe: [false]
+    });
+  }
+
+  onSubmit() {
+    console.log(this.form.value);
+  }
+
+}
